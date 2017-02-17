93e805354d73df2bfb034a1e9002e36b80a0956d
Eric Chang <echang25@hotmail.com>
2017-02-17 18:03:47 -0500

    adding new files

---
+++ Home Again/DetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailTableViewCell.swift
+//  Home Again
+//
+//  Created by Eric Chang on 2/17/17.
+//  Copyright © 2017 Eric Chang. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Home Again/Library.swift
@@ -0,0 +1,9 @@
+//
+//  Library.swift
+//  Home Again
+//
+//  Created by Eric Chang on 2/17/17.
+//  Copyright © 2017 Eric Chang. All rights reserved.
+//
+
+import Foundation
